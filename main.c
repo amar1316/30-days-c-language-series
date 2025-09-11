a4130250d213fd658e04a481dcf7a0b4167b5980
Kindruth Edu <gvamarnath100@gmail.com>
2025-09-11 10:58:29 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,68 @@
+/* while loop in c */
+
+
+
+#include <stdio.h>
+
+int main()
+{
+   /* int i=0, sum=0;
+    while(i<=5){                    // printing 5 numbers using while loop
+        printf("%d\n",i);
+        sum +=i;
+        i++;
+    }
+    printf("%d\n",sum);
+    */
+
+    /*int i=1;
+    while(i<=50){
+        printf("%d  ",i);           //printing odd numbers between 1 and 50
+        i+=2;
+    }*/
+
+
+
+  /*  int num, sum = 0, digit;
+    printf("Enter a number: ");
+    scanf("%d", &num);                              // finding sum of digits using while loop
+
+    while (num != 0) {
+        digit = num % 10;  // Extract the last digit
+        sum += digit;      // Add it to the sum
+        num /= 10;         // Remove the last digit
+    }
+
+    printf("Sum of digits: %d\n", sum);
+*/
+
+
+    /*int num;
+
+    printf("Enter number (0 to stop) : ");
+    scanf("%d",&num);                                    // o to stop program using while loop
+    while(num!=0){
+        printf("you entered : %d\n",num);
+         scanf("%d",&num);
+
+    }*/
+
+
+
+   /*
+   printf("\n\n--factorial program--\n\n");
+   int num, factorial = 1;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int i = 1;
+    while (i <= num) {
+        factorial *= i;  // Multiply factorial by the current value of i
+        i++;
+    }
+
+    printf("Factorial of %d is %d\n", num, factorial);
+*/
+
+    return 0;
+}
