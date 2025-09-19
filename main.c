993ffc0d5484f1e0235dedd400f9bffaf83dbe44
Kindruth Edu <gvamarnath100@gmail.com>
2025-09-19 14:25:36 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+/* Variables and data types in C*/
+
+#include <stdio.h>
+
+int main()
+
+{
+
+
+    printf("Integer: %d\n", 100);
+	printf("Float: %f\n", 3.14);
+	printf("Character: %c\n",'A');
+	printf("String: %s\n", "Hello World");
+
+
+    return 0;
+}
+
+
