39aad06d7aa835db19a2a6102e66713f8b71343a
Kindruth Edu <gvamarnath100@gmail.com>
2025-09-27 13:42:42 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+/* Pointers in C (part 1 - Basics of Pointers )*/
+
+#include <stdio.h>
+
+
+int main()
+{
+  int a =10 ;
+  int *ptr;
+  ptr = &a;
+
+  printf("Value of a : %d\n",a);
+  printf("Address of a : %p\n",&a);
+  printf("Pointer ptr stores address : %p\n",ptr);
+  printf("Pointer *ptr stores value : %d\n",*ptr);
+
+    return 0;
+}
